f67dbd0ecd4c2ae3715f819b4b385364fe7f072a
Nagesh Raj <nageshraj@Nageshs-MacBook-Pro.local>
2025-12-01 19:46:42 +0530

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/nagesh-dev-portfolio',
+    images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
